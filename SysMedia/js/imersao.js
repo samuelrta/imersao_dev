04fffdc96ac3c054e3217b156544e842eb6396b2
unknown <samuelrta@hotmail.com>
2021-10-03 19:07:33 -0300

    Otimização e limpeza do código | Funcionalidade limpar campos e texto

--- SysMedia/js/imersao.js
+++ SysMedia/js/imersao.js
@@ -1,18 +1,16 @@
 function CalculaMedia() {
-  var notaPrimeiroElemento = document.getElementById("nota1");
-  var nota = notaPrimeiroElemento.value;
-  var notaPrimeiro = parseFloat(nota);
+  var notaPrimeiro = parseFloat(document.getElementById("nota1").value);
+  var notaSegundo = parseFloat(document.getElementById("nota2").value);
+  var notaTerceiro = parseFloat(document.getElementById("nota3").value);
 
-  var notaSegundoElemento = document.getElementById("nota2");
-  var nota = notaSegundoElemento.value;
-  var notaSegundo = parseFloat(nota);
-
-  var notaTerceiroElemento = document.getElementById("nota3");
-  var nota = notaTerceiroElemento.value;
-  var notaTerceiro = parseFloat(nota);
+// == Antes o passo a passo para pegar um valor do input ==
+  //var notaTerceiroElemento = document.getElementById("nota3");
+  //var nota = notaTerceiroElemento.value;
+  //var notaTerceiro = parseFloat(nota);
+// ======
 
   var somaMedia = ((notaPrimeiro + notaSegundo + notaTerceiro) / 4).toFixed(1);
-
+      
     if (somaMedia >= 7) {
       var elementoResultadoMediaAprovado = document.getElementById("ResultadoMediaAprovado");
       var resultadoMedia = "Sua média é " + somaMedia + " - APROVADO"
@@ -23,6 +21,15 @@ function CalculaMedia() {
       elementoResultadoMediaReprovado.innerHTML = resultadoMedia
     }    
 }
-  
-  
-  
+
+function Limpar(){
+    document.getElementById("nota1").value = "";
+    document.getElementById("nota2").value = "";
+    document.getElementById("nota3").value = "";
+
+    elementoResultadoMediaReprovado = document.getElementById("ResultadoMediaReprovado");
+    elementoResultadoMediaAprovado = document.getElementById("ResultadoMediaAprovado");
+    resultadoMedia = ""
+    elementoResultadoMediaReprovado.innerHTML = resultadoMedia
+    elementoResultadoMediaAprovado.innerHTML = resultadoMedia
+  }
